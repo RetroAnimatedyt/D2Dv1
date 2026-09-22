f476e36c295e728639d3501d6b4916e976276fea
Michael <theretroanimatedyt@gmail.com>
2026-09-21 21:38:52 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: './', // Ensures assets load correctly on GitHub Pages
+  plugins: [react(), tailwindcss()],
 })
